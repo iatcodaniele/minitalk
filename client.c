6c149e0568c3e1c4f4061ee637137ba9bab4a875
Daniele <iatcodaniele@gmail.com>
2024-04-19 15:48:14 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -6,11 +6,12 @@
 /*   By: diatco <diatco@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:32:24 by diatco            #+#    #+#             */
-/*   Updated: 2024/04/19 13:51:20 by diatco           ###   ########.fr       */
+/*   Updated: 2024/04/19 15:46:54 by diatco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <stdio.h>
 
 static void	send_bits(pid_t pid, char c)
 {
@@ -19,7 +20,7 @@ static void	send_bits(pid_t pid, char c)
 	bit = 0;
 	while (bit < 8)
 	{
-		if ((c & (0x01 << bit)) != 0)
+		if (c & (0x01 << bit))
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
@@ -49,4 +50,29 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	return (0);
+
+    // int		i;
+	// int		s_pid;
+	// char	*octet;
+
+	// i = 0;
+	// if (argc != 3)
+	// {
+	// 	printf("You did something wrong...\n");
+	// 	return (1);
+	// }
+	// s_pid = ft_atoi(argv[1]);
+	// if (s_pid <= 0)
+	// {
+	// 	printf("What about the PID");
+	// 	return (1);
+	// }
+	// octet = argv[2];
+	// while (*octet)
+	// {
+	// 	sig_killer(s_pid, *octet);
+	// 	octet++;
+	// }
+	// sig_killer (s_pid, '\0');
+	// return (0);
 }
--- server.c
+++ server.c
@@ -6,19 +6,18 @@
 /*   By: diatco <diatco@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:32:15 by diatco            #+#    #+#             */
-/*   Updated: 2024/04/19 13:39:56 by diatco           ###   ########.fr       */
+/*   Updated: 2024/04/19 15:47:30 by diatco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <stdio.h>
 
 static void	sig_handler(int signum)
 {
 	static int	i;
 	static int	bit;
 
-	i = 0;
-	bit = 0;
 	if (signum == SIGUSR1)
 		i |= (0x01 << bit);
 	bit++;
@@ -32,17 +31,18 @@ static void	sig_handler(int signum)
 
 int	main(int ac, char **av)
 {
-	struct sigaction	sa;
 	pid_t				pid;
 	char				*pid_str;
+	struct sigaction	sa;
 
-	sa.sa_handler = sig_handler;
 	(void)av;
 	if (ac != 1)
 	{
 		write(1, "Error: wrong format.\n", 21);
 		write(1, "Try: ./server\n", 14);
 	}
+    sa = (struct sigaction){0};
+	sa.sa_handler = sig_handler;
 	pid = getpid();
 	pid_str = ft_itoa(pid);
 	write(1, "Server PID: ", 12);
